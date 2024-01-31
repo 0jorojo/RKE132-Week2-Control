173e7f187b078dd50492bb9bb90796f3cff1290b
0jorojo <nikita.kulakov@tktk.ee>
2024-01-31 20:57:16 +0200

    add week2 code

---
+++ UserAgeValidation/Program.cs
@@ -0,0 +1,36 @@
+﻿//ask for age, restrict access
+
+Console.WriteLine("How old are you?");
+
+string age = Console.ReadLine();
+int ageNum = 0;
+bool isAgeNum = Int32.TryParse(age, out ageNum);
+
+Console.WriteLine($"Parse result {isAgeNum}. User is {ageNum} y.o.");
+
+if(isAgeNum == true)
+{
+    if (ageNum >= 13)
+    {
+        Console.WriteLine("Welcome to heck!");
+    }
+    else
+    {
+        Console.WriteLine("Go away!");
+    }
+}
+else
+{
+    Console.WriteLine("Does not compute.");
+}
+
+//int age = Int32.Parse(Console.ReadLine());
+
+//if (age >= 13)
+//{
+//  Console.WriteLine("Welcome to heck!");
+//}
+//else
+//{
+//   Console.WriteLine("Go away!");
+//}
---
+++ UserGenderValidation/Program.cs
@@ -0,0 +1,20 @@
+﻿//ask gender & surname, greet by both
+
+Console.Write("Please select your gender (m/f):");
+char gender = Char.Parse(Console.ReadLine());
+
+Console.WriteLine("Please enter your surname:");
+string name = Console.ReadLine();
+
+if (gender == 'm')
+{
+    Console.WriteLine($"Greetings, Mr. {name}!");
+}
+else if (gender == 'f')
+{
+    Console.WriteLine($"Greetings, Ms. {name}!");
+}
+else
+{
+    Console.WriteLine($"Welcome, {name}!");
+}
